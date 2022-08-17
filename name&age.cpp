5453d953241e37b5682f802999de862fc0acb598
Niranjan307 <sonyniranjan307@gmail.com>
2022-08-17 23:10:19 +0530

    commit1

---
+++ name&age.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string yourName;
+	cout << "Enter your name: ";
+	getline(cin, yourName);
+	int age;
+	cout<<"Enter your age: ";
+	cin>>age;
+	cout <<"name: "<<yourName <<"\nage: "<<age;
+	
+}
